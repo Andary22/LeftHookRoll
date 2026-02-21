fc429061fb3168c4fc68f56221eed91ceb230990
dotacow <ykitaneh22@gmail.com>
2026-02-21 03:14:18 +0300

    init:Excpetions

---
+++ includes/FatalExceptions.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+/** @file FatalExceptions.hpp
+	@brief This class will be used to collect *fatal* exceptions, which are exceptions that should cause the program to exit immediately.
+	any exception that can be recovered from/used to control flow SHOULD NOT be in this file, and rather should be a subclass of it's cause class.
+*/
