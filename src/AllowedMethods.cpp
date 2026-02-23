e96379c5b6d447871d294ea31d41938382ba9127
dotacow <ykitaneh22@gmail.com>
2026-02-23 21:58:31 +0300

    allowed methods: implemented class

---
+++ src/AllowedMethods.cpp
@@ -0,0 +1,43 @@
+#include "../includes/AllowedMethods.hpp"
+
+// --- Canonical Form ---
+
+AllowedMethods::AllowedMethods() : _bitmap(0) {}
+
+AllowedMethods::AllowedMethods(const AllowedMethods& other) : _bitmap(other._bitmap) {}
+
+AllowedMethods& AllowedMethods::operator=(const AllowedMethods& other)
+{
+	if (this != &other)
+		_bitmap = other._bitmap;
+	return *this;
+}
+
+AllowedMethods::~AllowedMethods() {}
+
+// --- Bitwise Operations ---
+
+void AllowedMethods::addMethod(HTTPMethod method)
+{
+	_bitmap |= method;
+}
+
+void AllowedMethods::removeMethod(HTTPMethod method)
+{
+	_bitmap &= ~method;
+}
+
+bool AllowedMethods::isAllowed(HTTPMethod method) const
+{
+	return (_bitmap & method) != 0;
+}
+
+void AllowedMethods::clear()
+{
+	_bitmap = 0;
+}
+
+short AllowedMethods::getBitmap() const
+{
+	return _bitmap;
+}
