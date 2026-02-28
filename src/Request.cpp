fd2a73178b267436acd791f79de93449380f2e09
Yaman Alrifai <yaman.alrfai@gmail.com>
2026-03-01 01:41:42 +0300

    fix: constroctor

--- src/Request.cpp
+++ src/Request.cpp
@@ -24,13 +24,13 @@ namespace RequestUtils
 }
 // Canonical Form
 
-Request::Request(): _methodName(UNKNOWN_METHOD), _contentLength(-1), _reqState(REQ_HEADERS), _statusCode("200"), _maxBodySize(0), _totalBytesRead(0), _chunkSize(0), _chunkDecodeOffset(0), _isBodyProcessed(false)
+Request::Request(): _methodName(UNKNOWN_METHOD), _contentLength(-1), _reqState(REQ_HEADERS), _statusCode("200"), _maxBodySize(0), _totalBytesRead(0), _chunkSize(0), _chunkDecodeOffset(0), _isBodyProcessed(false), _ramParsePos(0)
 {}
 
-Request::Request(long long maxBodySize): _methodName(UNKNOWN_METHOD), _contentLength(-1), _reqState(REQ_HEADERS), _statusCode("200"), _maxBodySize(static_cast<size_t>(maxBodySize)), _totalBytesRead(0), _chunkSize(0), _chunkDecodeOffset(0), _isBodyProcessed(false)
+Request::Request(long long maxBodySize): _methodName(UNKNOWN_METHOD), _contentLength(-1), _reqState(REQ_HEADERS), _statusCode("200"), _maxBodySize(static_cast<size_t>(maxBodySize)), _totalBytesRead(0), _chunkSize(0), _chunkDecodeOffset(0), _isBodyProcessed(false), _ramParsePos(0)
 {}
 
-Request::Request(const Request& other): _methodName(other._methodName), _URL(other._URL), _protocol(other._protocol), _query(other._query), _contentLength(other._contentLength), _body(other._body), _headers(other._headers), _reqState(other._reqState), _statusCode(other._statusCode), _maxBodySize(other._maxBodySize), _totalBytesRead(other._totalBytesRead), _chunkSize(other._chunkSize), _chunkDecodeOffset(other._chunkDecodeOffset), _isBodyProcessed(other._isBodyProcessed)
+Request::Request(const Request& other): _methodName(other._methodName), _URL(other._URL), _protocol(other._protocol), _query(other._query), _contentLength(other._contentLength), _body(other._body), _decodedBody(other._decodedBody), _headers(other._headers), _reqState(other._reqState), _statusCode(other._statusCode), _maxBodySize(other._maxBodySize), _totalBytesRead(other._totalBytesRead), _chunkSize(other._chunkSize), _chunkDecodeOffset(other._chunkDecodeOffset), _isBodyProcessed(other._isBodyProcessed), _chunkBuffer(other._chunkBuffer), _ramParsePos(other._ramParsePos)
 {
 }
 
@@ -44,6 +44,7 @@ Request& Request::operator=(const Request& other)
 		_query = other._query;
 		_contentLength = other._contentLength;
 		_body = other._body;
+		_decodedBody = other._decodedBody;
 		_headers = other._headers;
 		_reqState = other._reqState;
 		_statusCode = other._statusCode;
@@ -52,6 +53,8 @@ Request& Request::operator=(const Request& other)
 		_chunkSize = other._chunkSize;
 		_chunkDecodeOffset = other._chunkDecodeOffset;
 		_isBodyProcessed = other._isBodyProcessed;
+		_chunkBuffer = other._chunkBuffer;
+		_ramParsePos = other._ramParsePos;
 	}
 	return *this;
 }
